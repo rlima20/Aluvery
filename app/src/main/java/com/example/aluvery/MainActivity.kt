4faac999654f8ffe77114581a809e58c8d033a9a
CIANDT\raphaellima <raphael.lima-ext@ab-inbev.com>
2023-02-18 22:22:05 -0300

    [Aluvery] - Building composables

--- app/src/main/java/com/example/aluvery/MainActivity.kt
+++ app/src/main/java/com/example/aluvery/MainActivity.kt
@@ -7,13 +7,17 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -23,8 +27,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.aluvery.ui.theme.AluveryTheme
 import com.example.aluvery.ui.theme.Purple500
 import com.example.aluvery.ui.theme.Teal200
@@ -54,34 +62,55 @@ fun Greeting(name: String) {
 @Preview(showBackground = true)
 @Composable
 fun ProductItem() {
-    Column(
-        modifier = Modifier
-            .height(250.dp)
-            .width(200.dp)
+    Surface(
+        modifier = Modifier.padding(8.dp),
+        shape = RoundedCornerShape(15.dp),
+        elevation = 4.dp
     ) {
-        Box(
+        Column(
             modifier = Modifier
-                .height(100.dp)
-                .background(
-                    brush = Brush.horizontalGradient(
-                        colors = listOf(
-                            Purple500,
-                            Teal200
+                .heightIn(250.dp, 300.dp)
+                .width(200.dp)
+        ) {
+            Box(
+                modifier = Modifier
+                    .height(100.dp)
+                    .background(
+                        brush = Brush.horizontalGradient(
+                            colors = listOf(
+                                Purple500,
+                                Teal200
+                            )
                         )
                     )
+                    .fillMaxWidth()
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_launcher_background),
+                    contentDescription = null,
+                    Modifier
+                        .offset(y = (50).dp)
+                        .size(100.dp)
+                        .clip(shape = CircleShape)
+                        .align(alignment = Alignment.BottomCenter)
                 )
-                .fillMaxWidth()
-        )
-        Image(
-            painter = painterResource(id = R.drawable.ic_launcher_background),
-            contentDescription = null,
-            Modifier
-                .offset(y = (-50).dp)
-                .size(100.dp)
-                .clip(shape = CircleShape)
-                .align(alignment = Alignment.CenterHorizontally)
-        )
-        Text(text = "Texto 1")
-        Text(text = "Texto 2")
+            }
+            Spacer(modifier = Modifier.height(50.dp))
+            Column(Modifier.padding(16.dp)) {
+
+                Text(
+                    text = LoremIpsum(50).values.first(),
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight(700),
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Text(
+                    text = "R$ 14,99",
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight(400)
+                )
+            }
+        }
     }
 }
